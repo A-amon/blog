92b7c459ad46349678a19d2bfcdfbb480b5c00e0
A-amon <64846892+A-amon@users.noreply.github.com>
2021-08-18 16:03:25 +0800

    Added modal accessibilities

--- src/components/ThankYouModal/index.js
+++ src/components/ThankYouModal/index.js
@@ -1,10 +1,18 @@
+import { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import SuccessImg from '@assets/images/success.jpg'
 
 const ThankYouModal = ({ show, onClose }) => {
+    const buttonRef = useRef(null)
+
+    useEffect(() => {
+        if (show)
+            buttonRef.current.focus()
+    }, [show])
+
     return (
         <div className={`thanks__modal ${show ? 'show' : ''}`}>
-            <div className="thanks__dialog">
+            <div className="thanks__dialog" role='dialog' aria-label='Successful subscription'>
                 <div className="thanks__image">
                     <img className='thanks__image-img' src={SuccessImg} alt="" aria-hidden="true" />
                 </div>
@@ -14,7 +22,7 @@ const ThankYouModal = ({ show, onClose }) => {
                         You are now subscribed to our blog updates.
                         No more late news.
                     </p>
-                    <button className="thanks__body-close" onClick={onClose}>
+                    <button ref={buttonRef} className="thanks__body-close" onClick={onClose}>
                         Got it
                     </button>
                 </div>
